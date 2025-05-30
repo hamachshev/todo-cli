1a1dbba4a8190e31b78fa5a8318b91491dc3db9f
Aharon Seidman <aharondov1@gmail.com>
2025-05-30 19:14:22 -0400

    can add todos and read them back using bincode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::{Parser, Subcommand};
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter, Write},
+};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -25,21 +29,20 @@ enum Commands {
 }
 
 fn main() {
-    let mut todos = Vec::new();
-
-    load_todos(&mut todos);
+    let mut todos = load_todos();
 
     let cli = Cli::parse();
 
     match &cli.command {
         Some(Commands::Add { item }) => {
             println!("adding item: {}", item);
-            todos.push(item.to_string());
+            todos.push(format!("- {}", item).to_string());
+            save_todos(&todos);
         }
         Some(Commands::List) => {
             println!("Listing all todos:");
             for todo in &todos {
-                println!("- {}", todo);
+                println!("{}", todo);
             }
         }
         Some(Commands::Mark { item }) => {
@@ -49,4 +52,26 @@ fn main() {
     }
 }
 
-fn load_todos(todos: &[String]) -> _ {}
+fn load_todos() -> Vec<String> {
+    let file_path = "todos.bin";
+    if let Ok(mut file) = File::open(file_path) {
+        println!("Loading existing todo list from {}", file_path);
+
+        let todos_read: Vec<String> =
+            bincode::decode_from_std_read(&mut file, bincode::config::standard()).expect(
+                "Failed to deserialize todo list. Ensure the file is in the correct format.",
+            );
+        return todos_read;
+    } else {
+        println!("No existing todo list found, starting fresh.");
+        return Vec::new();
+    }
+}
+
+fn save_todos(todos: &[String]) -> () {
+    let file_path = "todos.bin";
+    let mut file = File::create(file_path).expect("Failed to open todo file for writing");
+    // let mut writer = BufWriter::new(file);
+    bincode::encode_into_std_write(todos, &mut file, bincode::config::standard())
+        .expect("Failed to serialize todo list");
+}
