07e11c6f5ca5134e4d8c58b359218b1261158a11
Aharon Seidman <aharondov1@gmail.com>
2025-05-31 22:57:31 -0400

    can mark todos as done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 use std::{
     fs::File,
-    io::{BufReader, BufWriter, Write},
+    io::{BufReader, BufWriter},
 };
 
 #[derive(Parser)]
@@ -23,8 +23,8 @@ enum Commands {
     },
     /// Mark a done todo item
     Mark {
-        /// Item to add to the todo list
-        item: String,
+        /// Number of the item to mark as done
+        item: u16,
     },
 }
 
@@ -40,26 +40,36 @@ fn main() {
             save_todos(&todos);
         }
         Some(Commands::List) => {
-            println!("Listing all todos:");
-            for todo in &todos {
-                println!("{}", todo);
-            }
+            println!("listing all todos:");
+            list_todos(&todos);
         }
         Some(Commands::Mark { item }) => {
             println!("marking item as done: {}", item);
+            let index = *item as usize - 1;
+            todos[index] = format!("x {}", &todos[index][2..]);
+            list_todos(&todos);
+            save_todos(&todos);
         }
         None => {}
     }
 }
 
+fn list_todos(todos: &[String]) {
+    for (index, todo) in todos.iter().enumerate() {
+        println!("{}. {}", (index + 1), todo);
+    }
+}
 fn load_todos() -> Vec<String> {
     let file_path = "todos.bin";
-    if let Ok(mut file) = File::open(file_path) {
+    if let Ok(file) = File::open(file_path) {
         println!("Loading existing todo list from {}", file_path);
-
+        let mut reader = BufReader::new(file);
         let todos_read: Vec<String> =
-            bincode::decode_from_std_read(&mut file, bincode::config::standard()).expect(
-                "Failed to deserialize todo list. Ensure the file is in the correct format.",
+            bincode::decode_from_std_read(&mut reader, bincode::config::standard()).unwrap_or_else(
+                |_| {
+                    println!("Failed to deserialize todo list. Starting fresh.");
+                    Vec::new()
+                },
             );
         return todos_read;
     } else {
@@ -68,10 +78,10 @@ fn load_todos() -> Vec<String> {
     }
 }
 
-fn save_todos(todos: &[String]) -> () {
+fn save_todos(todos: &[String]) {
     let file_path = "todos.bin";
-    let mut file = File::create(file_path).expect("Failed to open todo file for writing");
-    // let mut writer = BufWriter::new(file);
-    bincode::encode_into_std_write(todos, &mut file, bincode::config::standard())
+    let file = File::create(file_path).expect("Failed to open todo file for writing");
+    let mut writer = BufWriter::new(file);
+    bincode::encode_into_std_write(todos, &mut writer, bincode::config::standard())
         .expect("Failed to serialize todo list");
 }
